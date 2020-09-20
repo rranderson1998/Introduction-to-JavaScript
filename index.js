8e43b9870507733d76ae991fd0709ab5be85ae77
rranderson1998 <rranderson1998@gmail.com>
2020-09-20 19:59:12 -0400

    lost

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+let votingAge
+let age
 
-
-
+if (age > 18) {
+    votingAge = "true"
+}
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
